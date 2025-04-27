9ec72efcaa2e41a6a635064062123f38492003f1
fddemarco <franco.demarco3400@gmail.com>
2025-04-26 22:32:28 -0300

    feat: add data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+
+const CATFACT_ENDPOINT = 'https://catfact.ninja/fact'
 
 export function App () {
-  const [fact, useFact] = useState('Lorem, ipsum dolor sit amet consectetur adipisicing elit.')
+  const [fact, setFact] = useState('Lorem, ipsum dolor sit amet consectetur adipisicing elit.')
+  useEffect(() => {
+    fetch(CATFACT_ENDPOINT)
+      .then(res => res.json())
+      .then(data => setFact(data.fact))
+  }, [])
   return (
     <main>
       <h1>Hello Cat!</h1>
