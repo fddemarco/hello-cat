4130e8d9cb9a93e6873d77a09cbf670e85dfeb41
fddemarco <franco.demarco3400@gmail.com>
2025-04-26 22:12:45 -0300

    chore: clean app

--- main.jsx
+++ main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
+import { App } from './src/App.jsx'
 
 document.body.innerHTML = '<div id="app"></div>'
 
 const root = createRoot(document.getElementById('app'))
-root.render(<h1>Hello, world</h1>)
+root.render(<App />)
